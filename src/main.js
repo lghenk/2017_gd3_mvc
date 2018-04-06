762d31afcee31d9a1bd5398377b901b8d1d5a6bd
Berend Weij <info@berendweij.nl>
2018-04-06 09:20:34 +0200

    adds working json url

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import HighscoreModel from './model/highscores.model';
 import HighscoreView from './view/highscores.view';
 
 const highscoreModel = new HighscoreModel();
-highscoreModel.load('http://localhost:3000/highscores');
+highscoreModel.load('http://www.studiozoetekauw.nl/ma/highscores.php');
 
 const highscoreView = new HighscoreView(highscoreModel);
-
